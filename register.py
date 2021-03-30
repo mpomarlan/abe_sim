302d6bec5b0c80d679b0b1d0184aca2a0302c0f3
Mihai Pomarlan <mpomarlan@yahoo.co.uk>
2021-03-30 14:13:31 +0200

    Fixes for installation on newer MORSE.

---
+++ register.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+
+import os 
+from os.path import expanduser
+
+ownPath = os.path.dirname(os.path.realpath(__file__))
+home = expanduser("~")
+morseDir = os.path.join(home,".morse")
+morseConfig = os.path.join(morseDir,"config")
+
+if not os.path.isfile(morseConfig):
+    if not os.path.isdir(morseDir):
+        os.mkdir(morseDir)
+    with open(morseConfig,"w") as outfile:
+        outfile.write("[sites]\nabe_sim=%s\n" % ownPath)
+else:
+    abeLine = "abe_sim=%s" % ownPath
+    txt = open(morseConfig).read()
+    if abeLine not in txt:
+        lines = txt.splitlines()
+        with open(morseConfig,"w") as outfile:
+            for l in lines:
+                outfile.write("%s\n" % l)
+                if "[sites]" == l.strip():
+                    outfile.write("%s\n" % abeLine)
+
--- src/abe_sim/builder/actuators/head.py
+++ src/abe_sim/builder/actuators/head.py
@@ -1,8 +1,14 @@
 from morse.builder.creator import ActuatorCreator
+from morse.version import VERSION
 
 class Head(ActuatorCreator):
+    _classpath = "abe_sim.actuators.head.Head"
+    _blendname = "head"
     def __init__(self, name=None):
-        ActuatorCreator.__init__(self, name, \
-                                 "abe_sim.actuators.head.Head",\
-                                 "head")
+        if VERSION < "1.3":
+            ActuatorCreator.__init__(self, name, \
+                                     "abe_sim.actuators.head.Head",\
+                                     "head")
+        else:
+            ActuatorCreator.__init__(self, name)
 
