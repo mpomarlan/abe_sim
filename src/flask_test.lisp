3d42bf83bd2a5402d57b4097cc47b0b5196a01a4
Jeroen Van Soest <jeroen.van.soest@vub.be>
2022-11-22 16:59:28 +0100

    api test lisp code and added a sample of the kitchen state json

--- src/flask_test.lisp
+++ src/flask_test.lisp
@@ -1,11 +1,9 @@
 (ql:quickload :drakma)
 (ql:quickload :cl-json)
 
-(json:encode-json
-  '#( ((kitchen . "aaaa") )))
+; encode json
+;(json:encode-json '#( ((kitchen . "aaaa") )))
 
-
-(drakma:http-request "http://lisp.org/")
 
 (defparameter *kitchen-host* "http://127.0.0.1:54321")
 
@@ -21,18 +19,75 @@
                      (error (e)
                        (format t "Error in response from spacy API service [nlp-tools penelope-interface]: ~S.~&" e))))))
 
-(send-request "/abe-sim-command/to-get-kitchen" (cl-json:encode-json-to-string `((:kitchen . "aaaaa" ))))
+;(send-request "/abe-sim-command/to-get-kitchen" (cl-json:encode-json-to-string `((:kitchen . "aaaaa" ))))
 
-(cl-json:encode-json-to-string 
-(send-request "/abe-sim-command/to-get-kitchen" (cl-json:encode-json-to-string `((:kitchen . "aaaaa" )))))
+; send request and show the string as json string again
+;(cl-json:encode-json-to-string (send-request "/abe-sim-command/to-get-kitchen" (cl-json:encode-json-to-string `((:kitchen . "aaaaa" )))))
 
 
-(cl-json:encode-json-to-string `((:kitchen . "aaaaa" )))
+
+; encode an association list to a json string
+;(cl-json:encode-json-to-string `((:kitchen . "aaaaa" )))
 
 ;retrieve key from association list
-(cdr (assoc (intern "a" :keyword) '((:a . 1 ))))
+;(cdr (assoc (intern "a" :keyword) '((:a . 1 ))))
 
-(defun get-kitchen-state (varname)
-  (send-request "/abe-sim-command/to-get-kitchen" (cl-json:encode-json-to-string `((:kitchen . ,varname )))))
 
-(get-kitchen-state "bbbb")
+
+(defun to-sprinkle ( object-assoc-list)
+  (send-request "/abe-sim-command/to-sprinkle" (cl-json:encode-json-to-string object-assoc-list)))
+
+
+(defun to-bake ( object-assoc-list)
+  (send-request "/abe-sim-command/to-bake" (cl-json:encode-json-to-string object-assoc-list)))
+
+(defun to-shape ( object-assoc-list)
+  (send-request "/abe-sim-command/to-shape" (cl-json:encode-json-to-string object-assoc-list)))
+
+(defun to-line ( object-assoc-list)
+  (send-request "/abe-sim-command/to-line" (cl-json:encode-json-to-string object-assoc-list)))
+
+(defun to-mix ( object-assoc-list)
+  (send-request "/abe-sim-command/to-mix" (cl-json:encode-json-to-string object-assoc-list)))
+
+(defun to-transfer ( object-assoc-list)
+  (send-request "/abe-sim-command/to-transfer" (cl-json:encode-json-to-string object-assoc-list)))
+
+(defun to-portion ( object-assoc-list)
+  (send-request "/abe-sim-command/to-portion" (cl-json:encode-json-to-string object-assoc-list)))
+
+(defun to-fetch ( object-assoc-list)
+  (send-request "/abe-sim-command/to-fetch" (cl-json:encode-json-to-string object-assoc-list)))
+
+(defun to-get-location ( object-assoc-list)
+  (send-request "/abe-sim-command/to-get-location" (cl-json:encode-json-to-string object-assoc-list)))
+
+(defun to-get-kitchen ( kitchen-state-variable)
+  (let ((response (send-request "/abe-sim-command/to-get-kitchen" (cl-json:encode-json-to-string `((:kitchen . ,kitchen-state-variable))))))
+    (when response (handler-case
+                       (assoc (intern (string-upcase kitchen-state-variable) :keyword) (cdr (assoc :response response)))
+                     (error (e) (format t "Error in response from abe-sim api service (route: to-get-kitchen): ~S.~&" e))))))
+
+
+(defun to-set-kitchen ( kitchen-state-in )
+   (send-request "/abe-sim-command/to-set-kitchen" (cl-json:encode-json-to-string `((:kitchen-state-in . ,kitchen-state-in)))))
+
+;;; command 1: get the kitchen state
+(defparameter  world-state (cdr (to-get-kitchen "?kitchen-state-1" )))
+
+
+;;; command 2: set the kitchen state
+(defparameter response-set-state (to-set-kitchen world-state))
+
+;;;command 3: get location
+;(defparameter response-get-location (to-get-location '((:available-location . "?available-countertop")(:type . "CounterTop") (:kitchen-State-In . nil) (:set-World-State . nil)) ))
+
+
+
+;response-get-location
+
+(cl-json:encode-json-to-string (cl-json:decode-json-from-string "{\"a\":[1,2]}"))
+
+world-state
+
+
