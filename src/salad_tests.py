f089f3b937127cdd3aa36ee29086dd14711e495a
Mihai Pomarlan <mpomarlan@yahoo.co.uk>
2023-04-25 17:32:50 +0200

    Added bacon to salad_tests scene.

--- src/salad_tests.py
+++ src/salad_tests.py
@@ -12,12 +12,13 @@ commands = [
         ["http://localhost:54321/abe-sim-command/to-fetch", {'object': 'largeBowl', 'kitchenStateIn': None, 'setWorldState': False}, None, "./sWS1.log"],
         ["http://localhost:54321/abe-sim-command/to-fetch", {'object': 'redOnion', 'kitchenStateIn': None, 'setWorldState': False}, "./sWS1.log", "./sWS2.log"],
         ["http://localhost:54321/abe-sim-command/to-fetch", {'object': 'broccoli', 'kitchenStateIn': None, 'setWorldState': False}, "./sWS2.log", "./sWS3.log"],
-        ["http://localhost:54321/abe-sim-command/to-cut", {'object': 'fetaCheese', 'cuttingTool': 'cookingKnife', 'cutPattern': 'dice', 'kitchenStateIn': None, 'setWorldState': False}, "./sWS3.log", "./sWS4.log"],
-        ["http://localhost:54321/abe-sim-command/to-cut", {'object': 'broccoli', 'cuttingTool': 'cookingKnife', 'cutPattern': 'dice', 'kitchenStateIn': None, 'setWorldState': False}, "./sWS4.log", "./sWS5.log"],
-        ["http://localhost:54321/abe-sim-command/to-cut", {'object': 'redOnion', 'cuttingTool': 'cookingKnife', 'cutPattern': 'dice', 'kitchenStateIn': None, 'setWorldState': False}, "./sWS5.log", "./sWS6.log"]
+        ["http://localhost:54321/abe-sim-command/to-cut", {'object': 'bacon', 'cuttingTool': 'cookingKnife', 'cutPattern': 'dice', 'kitchenStateIn': None, 'setWorldState': False}, "./sWS3.log", "./sWS4.log"],
+        ["http://localhost:54321/abe-sim-command/to-cut", {'object': 'fetaCheese', 'cuttingTool': 'cookingKnife', 'cutPattern': 'dice', 'kitchenStateIn': None, 'setWorldState': False}, "./sWS4.log", "./sWS5.log"],
+        ["http://localhost:54321/abe-sim-command/to-cut", {'object': 'broccoli', 'cuttingTool': 'cookingKnife', 'cutPattern': 'dice', 'kitchenStateIn': None, 'setWorldState': False}, "./sWS5.log", "./sWS6.log"],
+        ["http://localhost:54321/abe-sim-command/to-cut", {'object': 'redOnion', 'cuttingTool': 'cookingKnife', 'cutPattern': 'dice', 'kitchenStateIn': None, 'setWorldState': False}, "./sWS6.log", "./sWS7.log"]
     ]
 
-if crOp < 6:
+if crOp < 7:
     for k, e in enumerate(commands[crOp:]):
         command, req, pathIn, pathOut = e
         kSI = None
@@ -32,7 +33,7 @@ if crOp < 6:
             with open(pathOut, "w") as outfile:
                 _ = outfile.write("%s\n" % json.dumps(response['response']['kitchenStateOut']))
 
-dgr = {'kitchenStateIn': json.loads(open('sWS6.log').read())}
+dgr = {'kitchenStateIn': json.loads(open('sWS7.log').read())}
 r = requests.post("http://localhost:54321/abe-sim-command/to-set-kitchen", data=bytes(json.dumps(dgr), "utf-8"))
 response = json.loads(r.text)
 
@@ -40,7 +41,7 @@ dws = {'kitchenStateIn': '?kitchen-state-1'}
 r = requests.post("http://localhost:54321/abe-sim-command/to-get-kitchen", data=bytes(json.dumps(dws), "utf-8"))
 worldState = json.loads(r.text)['response']['?kitchen-state-1']
 
-toPlace = [x for x in worldState.keys() if (worldState[x]['type'] in ['ChoppedBroccoli', 'ChoppedRedOnion', 'FetaCheeseCube']) and ('kitchenCounter' == worldState[x]['at'])]
+toPlace = [x for x in worldState.keys() if (worldState[x]['type'] in ['ChoppedBroccoli', 'ChoppedRedOnion', 'FetaCheeseCube', 'ChoppedBacon']) and ('kitchenCounter' == worldState[x]['at'])]
 
 print(toPlace)
 for e in toPlace:
