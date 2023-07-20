b0d6285d8dc59927290b397a4a7f99494b91fdd7
Mihai Pomarlan <mpomarlan@yahoo.co.uk>
2023-07-20 17:01:01 +0200

    Output formats for json2Triples.py

--- src/json2Triples.py
+++ src/json2Triples.py
@@ -85,25 +85,79 @@ def json2Triples(data, eliminateRootS=False, nameProperty=None):
     return retq
 
 if "__main__" == __name__:
+    def _toO(o, ss):
+        if o in ss:
+            o = "<%s>" % o
+        else:
+            o = json.dumps(o)
+        return o
     parser = argparse.ArgumentParser(prog='json2Triples', description='Convert json files (e.g. abesim world dumps) into lists of triples', epilog='Text after help string goes here.')
     parser.add_argument('-i', '--inputFile', default="", help='Input json file to convert.')
     parser.add_argument('-o', '--outputFile', default="", help="Output file to write to.")
     parser.add_argument('-r', '--eliminateRootS', default=False, action="store_true", help='Do not return triples where the s component is /root/ i.e. where the s is the top-level json object.')
     parser.add_argument('-n', '--nameProperty', help='This property is used to define names for paths. Optional.')
+    parser.add_argument('-f', '--format', default="python", help="Specify format to return triples in. Options: python (triples returned as list of python lists; default); ntriples; turtle; xml.")
     arguments = parser.parse_args()
-    inputFile = arguments.inputFile
+    inputFile = str(arguments.inputFile)
+    formatT = str(arguments.format).lower().strip().strip('"').strip("'")
     if "" == inputFile:
         print("Must provide an input file")
-    outputFile = arguments.outputFile
+        sys.exit()
+    outputFile = str(arguments.outputFile)
     if "" == outputFile:
-        outputFile = inputFile[:inputFile.find(".")] + ".rdf"
+        idx = inputFile.find(".")
+        if -1 != idx:
+            outputFile = inputFile[:idx] + ".rdf"
+        else:
+            outputFile = inputFile + ".rdf"
     eliminateRootS = arguments.eliminateRootS
-    nameProperty = arguments.nameProperty
+    nameProperty = str(arguments.nameProperty)
     if "" == nameProperty:
         nameProperty = None
     data = json.loads(open(inputFile).read())
     res = json2Triples(data, eliminateRootS=eliminateRootS, nameProperty=nameProperty)
+    ss = set([str(x[0]) for x in res])
     with open(outputFile, "w") as outfile:
-        for e in res:
-            outfile.write("%s\n" % json.dumps(e))
+        if "turtle" == formatT:
+            outfile.write("@prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> .\n@prefix dc: <http://purl.org/dc/elements/1.1/> .\n@prefix owl: <http://www.w3.org/2002/07/owl#> .\n@prefix xml: <http://www.w3.org/XML/1998/namespace> .\n@prefix xsd: <http://www.w3.org/2001/XMLSchema#> .\n@prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#> .\n\n")
+            lastS = None
+            for e in res:
+                s, p, o = e
+                if (s != lastS):
+                    if (lastS is not None):
+                        outfile.write("  .\n<%s>\n" % s)
+                    else:
+                        outfile.write("<%s>" % s)
+                o = _toO(o,ss)
+                outfile.write("  <%s> %s ;\n" % (p, o))
+                lastS = s
+        elif "ntriples" == formatT:
+            for e in res:
+                o = _toO(e[2],ss)
+                outfile.write("<%s> <%s> %s .\n" % (e[0], e[1], o))
+        elif "xml":
+            outfile.write("<?xml version=\"1.0\"?>\n<rdf:RDF xmlns=\"\"\n         xmlns:owl=\"http://www.w3.org/2002/07/owl#\"\n         xmlns:rdf=\"http://www.w3.org/1999/02/22-rdf-syntax-ns#\"\n         xmlns:p=\"https://github.com/mpomarlan/abe_sim#\">\n")
+            lastS = None
+            for e in res:
+                s,p,o = e
+                try:
+                    aux = int(p)
+                    p = "Key_" + p
+                except ValueError:
+                    pass
+                if (lastS!=s):
+                    if lastS is not None:
+                        outfile.write("  </rdf:Description>\n")
+                    outfile.write("  <rdf:Description rdf:about=\"%s\">\n" % s)
+                if o in ss:
+                    if isinstance(p,int):
+                        print("Ohoh")
+                    outfile.write("    <p:%s rdf:resource=\"%s\" />\n" % (p, o))
+                else:
+                    outfile.write("    <p:%s>%s</p:%s>\n" % (p, _toO(o,ss), p))
+                lastS = s
+            outfile.write("</rdf:RDF>\n")
+        else:
+            for e in res:
+                outfile.write("%s\n" % json.dumps(e))
 
